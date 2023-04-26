e3455e540ab86e552addb4d3251bf96ee93c0ef5
Mampi Das <66109177+letsbecreative@users.noreply.github.com>
2023-04-26 21:27:14 +0530

    Add files via upload

---
+++ src/ToDoList.jsx
@@ -0,0 +1,44 @@
+import React, {useState} from "react";
+import AddIcon from '@mui/icons-material/Add';
+import DeleteIcon from '@mui/icons-material/Delete';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+
+
+const ToDoList = () => {
+
+    const [num, setNum] = useState(0);
+
+    const incNum = () => {
+        setNum(num+1);
+    }
+
+    const decNum = () => {
+        if(num > 0) {
+            setNum(num-1);
+        } else {
+            alert('Sorry, zero limit reached😢')
+            setNum(0);
+        }
+    };
+
+    return (
+        <>
+            <div className='main_div'>
+                <div className='center_div'>
+                    <h1> {num} </h1>
+                    <div className='btn_div'>
+                        <Tooltip title="Add">
+                        <Button onClick={incNum} className='btn_green'> <AddIcon /> </Button>
+                        </Tooltip>
+                        <Tooltip title="Delete">
+                        <Button onClick={decNum} className='btn_red'> <DeleteIcon /> </Button>
+                        </Tooltip>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+};
+
+export default ToDoList;
